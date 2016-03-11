2021bb0f39ef875ebc3cb1df0d0ff4183af31f9a
PhilSkot <philipp.skotnik@tech-port.de>
2016-03-11 19:24:36 +0100

    Create installer-withoutLEC.sh

---
+++ installer-withoutLEC.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#Dieses Script installiert die Essential Pakete & Konfiguiert den Server 
+
+#Umstellung auf Deutsch
+dpkg-reconfigure tzdata
+dpkg-reconfigure locales
+
+
+#Vorbereitung
+apt-get update
+apt-get dist-upgrade 
+
+#Installieren der Pakete
+apt-get -y install git mysql-server mysql-client php5-mysql phpmyadmin zip unzip figlet nano fail2ban mc perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
+
+#Installieren von Webmin
+wget http://www.webmin.com/download/deb/webmin-current.deb
+dpkg -i webmin-current.deb
+
+#Installieren von Java 8
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /etc/apt/sources.list.d/webupd8team-java.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
+apt-get update
+apt-get -y install oracle-java8-installer
+
+
